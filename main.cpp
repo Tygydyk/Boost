97b552cbd18c7b493993a155f3cdb8c2540bc94b
Tygydyk <32385649+Tygydyk@users.noreply.github.com>
2018-12-14 17:10:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
-#include <boost/filesystem/fstream.hpp>
-#include <string>
- 
-namespace fs = boost::filesystem;
- 
-int main(int argc, char* argv)
+#include "stdafx.h"
+#include <iostream>
+#include <boost/filesystem.hpp>
+using std::cout;
+using namespace boost::filesystem;
+
+int main(int argc, char* argv[])
 {
-  std::wstring filepath(L"C:\тест");
-  fs::ofstream(filepath);
-  return 0;
+	if (argc < 2)
+	{
+		cout << "Usage: tut3 path\n";
+		return 1;
+	}
+
+	path p (argv[1]);
+
+	try
+	{
+		if (exists(p))
+		{
+			if (is_regular_file(p))
+				cout << p << " size is " << file_size(p) << '\n';
+
+			else if (is_directory(p))
+			{
+				cout << p << " is a directory containing:\n";
+
+				for (directory_entry& x : directory_iterator(p))
+					cout << "    " << x.path() << '\n';
+			}
+			else
+				cout << p << " exists, but is not a regular file or directory\n";
+		}
+		else
+			cout << p << " does not exist\n";
+	}
+
+	catch (const filesystem_error& ex)
+	{
+		cout << ex.what() << '\n';
+	}
+
+	return 0;
 }
