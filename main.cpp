da06b6618514795a05ca9fe10b9c5d053fcaff9f
Tygydyk <32385649+Tygydyk@users.noreply.github.com>
2018-12-14 16:49:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <string>
+ 
+namespace fs = boost::filesystem;
+ 
+int main(int argc, char* argv)
+{
+  std::wstring filepath(L"C:\тест");
+  fs::ofstream(filepath);
+  return 0;
+}
